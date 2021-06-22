6f82ad971559d78f20224f1a9a989c97e85ba267
Akash Dwivedi <aakashdwivedi2000@gmail.com>
2021-06-22 12:51:58 +0530

    Prediction.js done

---
+++ Prediction.js
@@ -0,0 +1,15 @@
+let prediction=['You  will work hard to attract more money in my life. ','You  will worked hard and attracted more money.',
+    'You  will use surplus money to get a new house.',' You  will used surplus wealth to buy a new house.',
+   'You  will attract a new love into my life',
+   ' You  will have the courage to look for a better job.',
+   'There is no one better to be than you.',
+   'you will enough light .',
+   'you will get better every single day',
+   'you will be an amazing person.',
+   'All of your problems have solutions.'
+   ]
+
+
+let num=Math.floor(Math.random()*11);
+
+console.log('Your Prediction is '+prediction[num])
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Oracle Astrologist
+In this project we are developing an javascript script that generates random astrology predicition each time
+it is triggered. This is a part of the codecademy Fullstack project 
+
+
+### Tech stack
+1. JavaScript
+2. Git and GitHub
+3. Command line
